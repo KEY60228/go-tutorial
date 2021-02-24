6de2173b05468591390623e17563c05ee997c5e3
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-02-24 20:17:00 +0900

    4. Return a random greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // 引数nameへの挨拶を返す
@@ -13,6 +15,22 @@ func Hello(name string) (string, error) {
 	}
 
 	// ':=' は変数宣言と同時に代入する記法
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// package内の変数の初期化が終わったら呼ばれる？
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	// []stringでstring型のsliceの宣言
+	formats := []string {
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	// formatsの中からrandomなstringを返す
+	return formats[rand.Intn(len(formats))]
 }
