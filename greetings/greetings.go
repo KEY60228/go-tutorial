ab78530c5cc02229b81b5f31efe50021a2efef82
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-02-24 20:06:55 +0900

    3. Return and handle an error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,11 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // 引数nameへの挨拶を返す
 // 頭文字が大文字の関数は外部から使える(exportされた)関数
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// ':=' は変数宣言と同時に代入する記法
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
