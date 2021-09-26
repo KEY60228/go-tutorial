e7e4431bc88055ac505ce66c2388a82daeed8230
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-09-26 09:24:57 +0900

    editing pages

--- src/writing_web_applications/wiki.go
+++ src/writing_web_applications/wiki.go
@@ -15,6 +15,8 @@ type Page struct {
 func main() {
 	// ルーティング的な
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	// http.HandleFunc("/save/", saveHandler)
 	// サーバー起動？
 	// 予期せぬエラー時にlog吐く
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -40,4 +42,18 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>" +
+			"<form action\"/save/%s\" method=\"POST\">" + 
+			"<textarea name=\"body\">%s</textarea><br>" +
+			"<input type=\"submit\" value=\"Save\">" +
+			"</form>",
+		p.Title, p.Title, p.Body)
 }
