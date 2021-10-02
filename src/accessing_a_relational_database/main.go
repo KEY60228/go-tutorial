0d63b9575bfafc96ee3dc6a754c854bf8890af86
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-10-02 20:14:22 +0900

    get a database handle and connect

--- src/accessing_a_relational_database/main.go
+++ src/accessing_a_relational_database/main.go
@@ -1,5 +1,37 @@
 package main
 
-import "github.com/go-sql-driver/mysql"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/go-sql-driver/mysql"
+)
 
+var db *sql.DB
+
+func main() {
+	// DB接続
+	cfg := mysql.Config{
+		User: os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net: "tcp",
+		Addr: "mysql",
+		DBName: "recordings",
+	}
+
+	// DB handler
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err !=  nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	fmt.Println("Connected!")
+}
